c7dbaf9eb1d8e1d22b8906adcb525c3f43eb6f98
Kun Chen <kc75997@uga.edu>
2016-10-12 17:15:24 -0400

    Mostly UI changes 1. added About Page View Controller 2. added a scroll view to add a new task page with elements set by auto layout

---
+++ Task/AboutPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutPageViewController.swift
+//  Task
+//
+//  Created by Kun Chen on 10/12/16.
+//  Copyright © 2016 Kun Chen. All rights reserved.
+//
+
+import UIKit
+
+class AboutPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func BackBtnPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
